c28c9a049c36dd56567a64ff597c2732a386584f
Nuno Costa <nunopf99@gmail.com>
2018-01-06 02:07:46 +0000

    Edit the style of the secondary path

--- public_html/projects/league/index.php
+++ public_html/projects/league/index.php
@@ -316,7 +316,7 @@ lastMatchInfo();
                 <div class="lastgame">
                     <img src="./champs/<?php echo $lastChampNameURL ?>.png">
                     <img src="./perk/<?php echo $participantKeystone ?>.png">
-                    <img src="./perkStyle/<?php echo $participantSecondaryPath ?>.png">
+                    <img src="./perkStyle/<?php echo $participantSecondaryPath ?>.png" id="secondary">
                 </div>
                 <div class="lastgame">
                     <p>
