3c3f514bea26e389d26091f1da2962311b058e0d
Nuno Costa <nunopf99@gmail.com>
2018-01-22 23:43:38 +0000

    Trying new data formatting (1k, 1m) and giving game time in result pill

--- public_html/projects/league/index.php
+++ public_html/projects/league/index.php
@@ -1,5 +1,38 @@
 <?php
 
+// Converts a number into a short version, eg: 1000 -> 1k
+// Based on: http://stackoverflow.com/a/4371114
+function number_format_short( $n, $precision = 1 ) {
+	if ($n < 900) {
+		// 0 - 900
+		$n_format = number_format($n, $precision);
+		$suffix = '';
+	} else if ($n < 900000) {
+		// 0.9k-850k
+		$n_format = number_format($n / 1000, $precision);
+		$suffix = 'K';
+	} else if ($n < 900000000) {
+		// 0.9m-850m
+		$n_format = number_format($n / 1000000, $precision);
+		$suffix = 'M';
+	} else if ($n < 900000000000) {
+		// 0.9b-850b
+		$n_format = number_format($n / 1000000000, $precision);
+		$suffix = 'B';
+	} else {
+		// 0.9t+
+		$n_format = number_format($n / 1000000000000, $precision);
+		$suffix = 'T';
+	}
+  // Remove unecessary zeroes after decimal. "1.0" -> "1"; "1.00" -> "1"
+  // Intentionally does not affect partials, eg "1.50" -> "1.50"
+	if ( $precision > 0 ) {
+		$dotzero = '.' . str_repeat( '0', $precision );
+		$n_format = str_replace( $dotzero, '', $n_format );
+	}
+	return $n_format . $suffix;
+}
+
 // your summoner name here
 
 $summonerName = "NunoC99";
@@ -313,7 +346,7 @@ function lastMatchesInfo()
 
 function lastMatchInfo()
 {
-	global $region, $riotapikey, $lastMatchID, $summonerID, $champNameURL1, $champNameURL2, $champNameURL3, $participantDamageDealt, $participantDeaths, $participantKills, $participantAssists, $participantGold, $participantCSMin, $participantWLresult, $participantWLcolor, $participantKeystone, $participantSecondaryPath;
+	global $region, $riotapikey, $lastMatchID, $summonerID, $champNameURL1, $champNameURL2, $champNameURL3, $participantDamageDealt, $participantDeaths, $participantKills, $participantAssists, $participantGold, $participantCSMin, $participantWLresult, $participantWLcolor, $participantKeystone, $participantSecondaryPath, $gametime;
 	$participantIDorder = $participantID = '';
 
 	// parse lastest match information from Riot API
@@ -334,11 +367,11 @@ function lastMatchInfo()
 	// fetch game win, game time, damage dealt, deaths, kills, assists, gold and cs by your player
 
 	$participantWL = $lastMatchInfoResult["participants"][$participantIDorder]["stats"]["win"];
-	$participantDamageDealt = $lastMatchInfoResult["participants"][$participantIDorder]["stats"]["totalDamageDealtToChampions"];
+	$participantDamageDealt = number_format_short($lastMatchInfoResult["participants"][$participantIDorder]["stats"]["totalDamageDealtToChampions"]);
 	$participantDeaths = $lastMatchInfoResult["participants"][$participantIDorder]["stats"]["deaths"];
 	$participantKills = $lastMatchInfoResult["participants"][$participantIDorder]["stats"]["kills"];
 	$participantAssists = $lastMatchInfoResult["participants"][$participantIDorder]["stats"]["assists"];
-	$participantGold = $lastMatchInfoResult["participants"][$participantIDorder]["stats"]["goldEarned"];
+	$participantGold = number_format_short($lastMatchInfoResult["participants"][$participantIDorder]["stats"]["goldEarned"]);
 	$participantCS = $lastMatchInfoResult["participants"][$participantIDorder]["stats"]["totalMinionsKilled"] + $lastMatchInfoResult["participants"][$participantIDorder]["stats"]["neutralMinionsKilledEnemyJungle"] + $lastMatchInfoResult["participants"][$participantIDorder]["stats"]["neutralMinionsKilledTeamJungle"];
 	$participantGameTime = $lastMatchInfoResult["gameDuration"];
 
@@ -349,8 +382,14 @@ function lastMatchInfo()
 	$participantSecondaryPath = $lastMatchInfoResult["participants"][$participantIDorder]["stats"]["perkSubStyle"];
 
 	// calculate cs per min
-
 	$participantCSMin = round($participantCS / $participantGameTime, 1);
+
+    // calculate game time in minutes
+
+	$participantGameTime = $lastMatchInfoResult["gameDuration"];
+    $participantGameTimeMin = intdiv($participantGameTime, 60);
+    $participantGameTimeSec = $participantGameTime % 60;
+    $gametime = $participantGameTimeMin .": ". $participantGameTimeSec;
 
 	// get color for the chip, red for loss, green for win
 
@@ -457,7 +496,7 @@ lastMatchInfo();
             <div class="centeredBox">
                 <div id="leagueIDfb">
                     <h2>My Last Game (<?php echo $lastMatchQueueReal ?>)</h2>
-                    <span style="background-color: <?php echo $participantWLcolor ?>"><?php echo $participantWLresult ?></span>
+                    <span style="background-color: <?php echo $participantWLcolor ?>"><?php echo $participantWLresult ."/ ". $gametime ?></span>
                 </div>
                 <div class="lastgame">
                     <img src="https://ddragon.leagueoflegends.com/cdn/7.24.2/img/champion/<?php echo $lastChampNameURL ?>.png">
