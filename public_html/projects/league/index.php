3a551808cb85a4ad2fb6bf90a398a2e2e6b46c5c
Nuno Costa <nunopf99@gmail.com>
2018-02-03 01:34:01 +0000

    test

--- public_html/projects/league/index.php
+++ public_html/projects/league/index.php
@@ -339,7 +339,7 @@ function lastMatchesInfo()
 		$lastMatchQueueReal = "Normal";
 	};
 	$lastChampName = $champNameSheetDecoded['data']["$lastMatchChampionID"]['name'];
-	$lastChampNameURL = preg_replace("/(\t|\n|\v|\f|\r| |\xC2\x85|\xc2\xa0|\xe1\xa0\x8e|\xe2\x80[\x80-\x8D]|\xe2\x80\xa8|\xe2\x80\xa9|\xe2\x80\xaF|\xe2\x81\x9f|\xe2\x81\xa0|\xe3\x80\x80|\xef\xbb\xbf)+/", "", $lastChampName);
+	$lastChampNameURL = preg_replace("/[^ \w]+/", "", $lastChampName);
 };
 
 // fetch last match info about the summoner in use
