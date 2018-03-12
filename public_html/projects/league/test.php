d9e0cb69c221f0b832eb649aa3f4d01dc8c6ebec
Nuno Costa <nunopf99@gmail.com>
2018-03-12 22:29:29 +0000

    Testing

--- public_html/projects/league/test.php
+++ public_html/projects/league/test.php
@@ -1,15 +1,5 @@
 <?php
-$region = 'euw1';
-
-// get riot api key from text file. if instead, you want to have it in the file, replace line 9 e 10 with:
-// $riotapikey = 'YOUR API KEY HERE';
-
-$riotkey = "RGAPI-42caac85-2bfc-453a-9be5-2d67848f2ee9";
-$riotapikey = $riotkey;
 $champNameSheet = file_get_contents("champions.json");
 $champNameSheetDecoded = json_decode($champNameSheet, true);
-$champID1= 145;
-$i = 0;
-    $flag = false;
-    echo $champNameSheetDecoded['data'][$i]["key"];
+echo '<pre>' . print_r($champNameSheetDecoded) . '</pre>';
 ?>
