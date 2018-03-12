a93471ba54e29cad9d0433209d7c953a8019822a
Nuno Costa <nunopf99@gmail.com>
2018-03-12 22:49:00 +0000

    test

--- public_html/projects/league/test.php
+++ public_html/projects/league/test.php
@@ -2,7 +2,7 @@
 $champNameSheet = file_get_contents("champions.json");
 $champNameSheetDecoded = json_decode($champNameSheet, true);
 foreach(champNameSheetDecoded['data'] as $key => $val) {
-      if ($val['key'] = 1){
+      if ($val['key'] = "1"){
           echo $key;
       }
 }
