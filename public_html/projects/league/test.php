9120e3397b221f95974883660482a846b1cb34e6
Nuno Costa <nunopf99@gmail.com>
2018-03-12 22:55:52 +0000

    test

--- public_html/projects/league/test.php
+++ public_html/projects/league/test.php
@@ -4,4 +4,9 @@ $array = json_decode($champNameSheet, true);
 echo "<pre>";
 echo print_r ($array);
 echo "</pre>";
+foreach ($array['data'] as $key => $value){
+    echo $key;
+    echo "<br>";
+    echo $value['id'];
+};
 ?>
