53a2ba001e692d44abad91f41f37cb4497b7d2e7
Nuno Costa <nunopf99@gmail.com>
2018-03-12 22:37:23 +0000

    test

--- public_html/projects/league/test.php
+++ public_html/projects/league/test.php
@@ -3,6 +3,8 @@ $champNameSheet = file_get_contents("champions.json");
 $champNameSheetDecoded = json_decode($champNameSheet, true);
 echo '<pre>' . print_r($champNameSheetDecoded) . '</pre>';
 echo print_r($champNameSheetDecoded["data"]["Aatrox"]);
-
-echo print_r($champNameSheetDecoded["data"]["0"]);
+echo "TETETETETET";
+echo print_r($champNameSheetDecoded["data"][0]);
+echo "TETETETETET";
+echo print_r($champNameSheetDecoded["data"][0]["id"]);
 ?>
